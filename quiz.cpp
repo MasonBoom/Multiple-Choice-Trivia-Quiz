698060f326a335c4f37f3f73aaa37eb0850ae50e
MasonBoom <masonboom@gmail.com>
2022-04-29 23:02:37 -0400

    using namespace std

--- quiz.cpp
+++ quiz.cpp
@@ -2,18 +2,20 @@
 #include <vector>
 #include <string>
 
+using namespace std;
+
 // defines how questions vector will be structured
 
 struct Question
 {
-    std::string question;
-    std::vector<std::string> answers;
+    string question;
+    vector<string> answers;
     int correct;
 };
 
 // list of questions and answers
 
-std::vector<Question> questions = 
+vector<Question> questions = 
 {
     { "What is Python", {"A large snake", "a high-level general-purpose programming language", "Both A and B", "None of the above"}, 'C'},  // 1
     { "How many blue stripes are on the USA flag", {"13", "0", "11", "7"}, 'B'},
@@ -35,44 +37,44 @@ std::vector<Question> questions =
 int main() {
     int correctCount = 0;  // var used to return total amount correct
     for (const auto& q: questions) {
-        std::cout << q.question << "?\n";
+        cout << q.question << "?\n";
         char option = 'A';
 
         // Creates A) B) C) D) up to Z) depending on the amount of options
 
         for (const auto& a: q.answers){
-            std::cout << option << ") " << a << '\n';
+            cout << option << ") " << a << '\n';
             option += 1;
         }
 
         // While loop will not let user proceed until a valid answer is given for every question
 
         while (option != 'A' && option != 'B' && option != 'C' && option != 'D') {
-            std::cout << "Please enter a valid option: ";
-            std::cin >> option;
+            cout << "Please enter a valid option: ";
+            cin >> option;
         }
 
         // score increased by 1 for every right answer
 
         if (option == q.correct) {
-            std::cout << "Correct!\n";
+            cout << "Correct!\n";
             correctCount += 1;
         } else {
-            std::cout << "Incorrect!\n";
+            cout << "Incorrect!\n";
         }
 
     }
 
     // Displays user score in a fraction and a percentage
 
-    std::cout << "You got " << correctCount << " out of " << questions.size() << " correct!\n";
-    std::cout << "Score: " << (double)correctCount / questions.size() * 100 << "%\n\n";
+    cout << "You got " << correctCount << " out of " << questions.size() << " correct!\n";
+    cout << "Score: " << (double)correctCount / questions.size() * 100 << "%\n\n";
 
     // Gives user the option to review incorrect questions after the quiz
 
-    std::cout << "Would you like to review the questions you got wrong? (y/n): ";
+    cout << "Would you like to review the questions you got wrong? (y/n): ";
     char yesOrNo;
-    std::cin >> yesOrNo;
+    cin >> yesOrNo;
 
     if (yesOrNo == 'y'){
 
@@ -82,17 +84,17 @@ int main() {
             char option = 'A';
             for (const auto& a: q.answers) {
                 if (option == q.correct) {
-                    std::cout << q.question << "?\n";
-                    std::cout << option << ") " << a << '\n';
+                    cout << q.question << "?\n";
+                    cout << option << ") " << a << '\n';
                 }
                 option += 1;
             }
         }
     } else if (yesOrNo == 'n') {
-        std::cout << "Thank you for playing!\n";
+        cout << "Thank you for playing!\n";
     } else {
-        std::cout << "Please enter a valid option: ";
-        std::cin >> yesOrNo;
+        cout << "Please enter a valid option: ";
+        cin >> yesOrNo;
     }
 
 }
